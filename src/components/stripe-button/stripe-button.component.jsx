23ead76a045ad72e5c9dad9333af2fb0b4c07159
Nathan Cerny <ncerny@gmail.com>
2021-01-20 08:26:05 -0600

    Integrate with Stripe, update title, update Favicon, update checkout flow.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51Hn1icBAxPTMaFG29WMhXQq0dtZDer5WiVb3faLbc13FnKxz4jCkfRJUTHvS7PttPEbFeRgnZ6IaoqnounPnCR5x00gBjLy80C';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddresss
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
