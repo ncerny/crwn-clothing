be427428b847114ce88b35fa5aca56264e035e8c
Nathan Cerny <ncerny@gmail.com>
2021-01-29 16:13:27 -0600

    Separate CRA app into client folder.  Added express BE server to handle backend api calls.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -8,7 +9,23 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = (token) => {
     console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment.  Please ensure you use the provided credit card number.'
+        );
+      });
   };
 
   return (
